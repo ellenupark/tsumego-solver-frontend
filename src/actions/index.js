1d8315dc528127b0fb416bc9f30f3b56cbe33b30
ellenupark <ellenupark@gmail.com>
2021-01-09 18:11:44 -0500

    Moved logic to action

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,8 @@ export const fetchProblem = () => {
         return fetch('http://localhost:3000/problems')
         .then(resp => resp.json())
         .then(problem => {
-            dispatch({ type: "SET_PROBLEM", payload: problem })
+            const board = {...problem.data[0].attributes, board: convertStringToBoard(problem.data[0].attributes.board, problem.data[0].attributes.size), answer: convertStringToBoard(problem.data[0].attributes.answer, problem.data[0].attributes.size)}
+            dispatch({ type: "SET_PROBLEM", payload: board })
         })
     }
 }
@@ -13,4 +14,35 @@ export const playMove = move => {
         type: "PLAY_MOVE",
         payload: move
     }
+}
+
+function convertStringToBoard(string, size) {
+    let  result = [];
+    let  count = 0;
+    for (let  i = 0; i < size; i++) {
+        result.push([])
+    }
+    for (let  i = 0; i < string.length; i++) {
+        if (string[i] !== '-') {
+            result[count].push(parseInt(string[i]))
+        } else {
+            count += 1;
+        }
+    }
+    return  result;
+}
+
+function convertBoardToString(array, size) {
+    let result = "";
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array[i].length; j++) {
+            if (j === size - 1 && i !== size - 1) {
+                result += (array[i][j].toString())
+                result += ('-')
+            } else {
+                result += (array[i][j].toString())
+            }
+        }
+    }
+    return result;
 }
--- src/reducers/boardReducer.js
+++ src/reducers/boardReducer.js
@@ -4,37 +4,6 @@
 
 // const board = new Board(9);
 
-function convertStringToBoard(string, size) {
-    let  result = [];
-    let  count = 0;
-    for (let  i = 0; i < size; i++) {
-        result.push([])
-    }
-    for (let  i = 0; i < string.length; i++) {
-        if (string[i] !== '-') {
-            result[count].push(parseInt(string[i]))
-        } else {
-            count += 1;
-        }
-    }
-    return  result;
-}
-
-function convertBoardToString(array, size) {
-    let result = "";
-    for (let i = 0; i < array.length; i++) {
-        for (let j = 0; j < array[i].length; j++) {
-            if (j === size - 1 && i !== size - 1) {
-                result += (array[i][j].toString())
-                result += ('-')
-            } else {
-                result += (array[i][j].toString())
-            }
-        }
-    }
-    return result;
-}
-
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state = [], action) => {
     switch (action.type) {
@@ -49,7 +18,8 @@ export default (state = [], action) => {
             newState.current_color === 1 ? newState.current_color = 2 : newState.current_color = 1
             return newState;
         case "SET_PROBLEM":
-            return {...action.payload.data[0].attributes, board: convertStringToBoard(action.payload.data[0].attributes.board, action.payload.data[0].attributes.size)}
+            debugger
+            return {...action.payload}
         default:
             return state
     }
