0550b8e434f5d0c2134358604c289b1c756967ff
ellenupark <ellenupark@gmail.com>
2021-01-10 17:18:48 -0500

    Added outline to stone depending on correct answer

--- src/components/BoardAnswerIntersections.js
+++ src/components/BoardAnswerIntersections.js
@@ -3,11 +3,35 @@ import React, { Component } from 'react';
 const GRID_SIZE = 40;
 
 class BoardAnswerIntersections extends Component {
+    checkForCorrectAnswer = (board) => {
+        for (let i = 0; i < board.answer.length; i++) {
+            for (let j = 0; j < board.answer.length; j++) {
+                if (board.answer[i][j] !== board.currentBoard[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    retrieveCorrectMove = (board) => {
+        let row = parseInt(board.move.split('')[0]);
+        let col = parseInt(board.move.split('')[1]);
+
+        return {row: row, col: col};
+    }
 
     render() {
-        let classes = "intersection";
+        const correct = this.checkForCorrectAnswer(this.props.board);
+        const stoneColor = this.props.board.player === "Black" ? ' black' : ' white';
+        const move = this.retrieveCorrectMove(this.props.board)
 
-        if (this.props.board.currentBoard[this.props.row][this.props.col] === 1) {
+        let classes = "intersection";
+        if (this.props.row === move.row && this.props.col === move.col && correct) {
+            classes += `${stoneColor} green`
+        } else if (this.props.board.currentBoard[this.props.row][this.props.col] !== this.props.board.board[this.props.row][this.props.col] && !correct) {
+            classes += `${stoneColor} red`
+        } else if (this.props.board.currentBoard[this.props.row][this.props.col] === 1) {
             classes += " black"
         } else if (this.props.board.currentBoard[this.props.row][this.props.col] === 2) {
             classes += " white"
