ba71d119a74b0c6dbdcaa6db9b0862c3061b6c23
ellenupark <ellenupark@gmail.com>
2021-03-16 21:32:05 -0400

    Updated loading spinner

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -4,8 +4,8 @@ import Spinner from 'react-bootstrap/Spinner'
 const LoadingSpinner = () => {
     return (
         <div className="loading-spinner">
-            <h3>Waking up dynos...</h3>
-            <h3>Thank you for your patience.</h3>
+            <h3>Preparing board...</h3>
+            <h3>Thank you for your patience</h3>
             <Spinner animation="border" role="status" />
         </div>
     );
