cd73064b11e7af8abc090d724025bc0af3c91c25
ellenupark <ellenupark@gmail.com>
2021-04-14 23:18:02 -0400

    Simplified conditional logic

--- src/components/BoardAnswerIntersections.js
+++ src/components/BoardAnswerIntersections.js
@@ -3,16 +3,7 @@ import React, { Component } from 'react';
 const GRID_SIZE = 40;
 
 class BoardAnswerIntersections extends Component {
-    checkForCorrectAnswer = (board) => {
-        for (let i = 0; i < board.answer.length; i++) {
-            for (let j = 0; j < board.answer.length; j++) {
-                if (board.answer[i][j] !== board.currentBoard[i][j]) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+    checkForCorrectAnswer = problem => JSON.stringify(problem.answer) === JSON.stringify(problem.currentBoard);
 
     retrieveCorrectMove = (board) => {
         let row = parseInt(board.move.split('-')[0]);
@@ -29,8 +20,6 @@ class BoardAnswerIntersections extends Component {
         let classes = "intersection";
         if (this.props.row === move.row && this.props.col === move.col && correct) {
             classes += `${stoneColor} green`
-        // } else if (this.props.row === move.row && this.props.col === move.col && !correct) {
-        //     classes += `${stoneColor} green`
         } else if (this.props.board.currentBoard[this.props.row][this.props.col] !== this.props.board.board[this.props.row][this.props.col] && !correct) {
             classes += `${stoneColor} red`
         } else if (this.props.board.currentBoard[this.props.row][this.props.col] === 1) {
--- src/components/DetailsButtons.js
+++ src/components/DetailsButtons.js
@@ -5,21 +5,16 @@ import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button'
 
 const DetailsButtons = (props) => {
-    if (props.errors) {
-        return (
-            <div>
-                <div className="submit-button" >
-                    <Link to={`/problems/${props.id}/answer`} style={{textDecoration: 'none'}}><Button  onClick={() => props.submitAnswer(props.problems.find(problem => problem.id === props.id))} variant="success" size="lg">Submit</Button>{' '}</Link>
-                </div>
+    return (
+        <div>
+            <div className="submit-button" >
+                <Link to={`/problems/${props.id}/answer`} style={{textDecoration: 'none'}}><Button  onClick={() => props.submitAnswer(props.problems.find(problem => problem.id === props.id))} variant="success" size="lg">Submit</Button>{' '}</Link>
+            </div>
+            {props.errors && (
                 <div className="error-message">
                     <div>{props.errors}</div>
                 </div>
-            </div>
-        );
-    }
-    return (
-        <div className="submit-button" >
-            <Link to={`/problems/${props.id}/answer`} style={{textDecoration: 'none'}}><Button  onClick={() => props.submitAnswer(props.problems.find(problem => problem.id === props.id))} variant="success" size="lg">Submit</Button>{' '}</Link>
+            )}
         </div>
     );
 }
