66fa290e8653cf4dfcc3b3241818755741b40dcc
ellenupark <ellenupark@gmail.com>
2021-04-14 23:15:04 -0400

    Revised correct answer check to use json

--- src/components/AnswerDetailsSummary.js
+++ src/components/AnswerDetailsSummary.js
@@ -1,31 +1,14 @@
 import React, { Component } from 'react';
 
 class AnswerDetailsSummary extends Component {
-    checkForCorrectAnswer = (board) => {
-        for (let i = 0; i < board.answer.length; i++) {
-            for (let j = 0; j < board.answer.length; j++) {
-                if (board.answer[i][j] !== board.currentBoard[i][j]) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+    checkForCorrectAnswer = problem => JSON.stringify(problem.answer) === JSON.stringify(problem.currentBoard);
 
     render() {
-        if (this.checkForCorrectAnswer(this.props.problem)) {
-            return (
-                <div>
-                    <h2>You are Correct!</h2>
-                </div>
-            );
-        } else {
-            return (
-                <div>
-                    <h2>Incorrect Answer!</h2>
-                </div>
-            );
-        }
+        return (
+            <div>
+                {this.checkForCorrectAnswer(this.props.problem) ? <h2>You are Correct!</h2> : <h2>Incorrect Answer!</h2>}
+            </div>
+        );
     }
 }
 
